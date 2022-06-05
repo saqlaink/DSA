2aeb5e4226066fba1e31b9ccea75c62202e3d1cd
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-05 15:02:53 +0530

    Arrays 3

---
+++ Arrays/NextGreaterPermutation.java
@@ -0,0 +1,51 @@
+// Given an array, rearrange it to its next greater permutation. Do it in-place with extra constant memory only. Do not use any library function for the next permutation.
+// If the next greater permutation does not exist, return the lowest possible order (sorted in ascending order).
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+   void nextGreaterPermutation (int[] arr) {
+       reverse(arr, 0, arr.length - 1);
+       if (isSorted(arr) == true) {
+           return;
+       }
+       int index = 0;
+       for (int i = 1; i < arr.length; i++) {
+           if (arr[i - 1] > arr[i]) {
+               index = i;
+               break;
+           }
+       }
+       for (int i = 0; i < index; i++) {
+           if (arr[i] > arr[index]) {
+               swap(arr, i, index);
+               break;
+           }
+       }
+       reverse(arr, 0, index - 1);
+       reverse(arr, 0, arr.length - 1);
+   }
+   boolean isSorted(int[] arr) {
+       boolean sorted = true;
+       for(int i = 1; i < arr.length; i++) {
+           if(arr[i] < arr[i - 1]) {
+               sorted = false;
+           }
+       }
+       return sorted;
+   }
+   void reverse(int[] arr, int start, int end) {
+       int left = start, right = end;
+       while(left < right) {
+           swap(arr, left, right);
+           left++;
+           right--;
+       }
+   }
+   
+   void swap(int[] arr, int first, int second) {
+       int temp = arr[first];
+       arr[first] = arr[second];
+       arr[second] = temp;
+   }
+}
