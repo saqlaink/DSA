2a8a6e5d5b41af76e5ce5f9ee94ad0bc1de8a2c4
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-06 09:42:48 +0530

    Array 9

--- Arrays/CyclicSort.java
+++ Arrays/CyclicSort.java
@@ -1,21 +1,22 @@
 // Find the duplicate in an array of N+1 integers
 // Problem Statement: Given an array of N + 1 size, where each element is between 1 and N. Assuming there is only one duplicate number, your task is to find the duplicate number.
+// Time complexity: O(N). Since we traversed through the array only once.
+// Space complexity: O(1).
 
 class Solution {
-    public int findDuplicate(int[] nums) {
-        int i=0;
-        while(i<nums.length){
-            if(nums[i] != i+1){
-                if(nums[i] != nums[nums[i] - 1]){
-                    swap(nums,i,nums[i]-1);
-                }else return nums[i];
-            }else i++;
-        }
-        return -1;
+  public int findDuplicate(int[] nums) {
+    int slow = nums[0];
+    int fast = nums[0];
+    do{
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    }while(slow!=fast);
+
+    fast = nums[0];
+    while(slow!=fast){
+      slow = slow.get(slow);
+      fast = fast.get(fast);
     }
-    public static void swap(int[] arr,int i1,int i2) {
-        var temp = arr[i1];
-        arr[i1] = arr[i2];
-        arr[i2] = temp;
-    }
+    return slow;
+  }
 }
