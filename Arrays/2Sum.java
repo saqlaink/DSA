087119512ef4b7e60f35a8f29b90c1f8aaf607ad
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-08 10:04:07 +0530

    Array 17

--- Arrays/2Sum.java
+++ Arrays/2Sum.java
@@ -1,3 +1,10 @@
+// Two Sum : Check if a pair with given sum exists in Array
+// Problem Statement: Given an array of integers nums[] and an integer target, return indices of the two numbers such that their sum is equal to the target.
+
+// Note: Assume that there is exactly one solution, and you are not allowed to use the same element twice. Example: If target is equal to 6 and num[1] = 3, then nums[1] + nums[1] = target is not a solution.
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map = new HashMap<>();
