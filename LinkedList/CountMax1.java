cb88a0315afde073a7965ece156abb7e720159ba
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-13 21:31:36 +0530

    LL 16

---
+++ LinkedList/CountMax1.java
@@ -0,0 +1,17 @@
+// Count Maximum Consecutive One’s in the array
+// Problem Statement: Given an array that contains only 1 and 0 return the count of maximum consecutive ones in the array.
+
+static int findMaxConsecutiveOnes(int nums[]) {
+        int cnt = 0;
+        int maxi = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+
+            maxi = Math.max(maxi, cnt);
+        }
+        return maxi;
+    }
