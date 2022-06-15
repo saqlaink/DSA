4ed7e4fa9ac594d8fbab1f1d35e2f912cedc1e6a
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-15 11:42:44 +0530

    Recursion

---
+++ Recursion And BackTracking/Subset2.java
@@ -0,0 +1,19 @@
+// Subset – II | Print all the Unique Subsets
+// Problem Statement: Given an array of integers that may contain duplicates the task is to return all possible subsets. Return only unique subsets and they can be in any order.
+
+public static void findSubsets(int ind, int[] nums, List<Integer> ds, List<List<Integer>> ansList) {
+        ansList.add(new ArrayList<>(ds)); 
+        for(int i = ind;i<nums.length;i++) {
+            if(i!=ind && nums[i] == nums[i-1]) continue; 
+            ds.add(nums[i]); 
+            findSubsets(i+1, nums, ds, ansList); 
+            ds.remove(ds.size() - 1);
+        }
+        
+    }
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums); 
+        List<List<Integer>> ansList = new ArrayList<>(); 
+        findSubsets(0, nums, new ArrayList<>(), ansList); 
+        return ansList; 
+    }
