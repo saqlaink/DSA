b6be47d232fe1a7426de72f841bc26cfacaf7961
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-15 11:41:39 +0530

    Recursion

---
+++ Recursion And BackTracking/CombinationSum.java
@@ -0,0 +1,32 @@
+// Combination Sum – 1
+// Problem Statement: 
+
+// Given an array of distinct integers and a target, you have to return the list of all unique combinations where the chosen numbers sum to target. You may return the combinations in any order.
+
+// The same number may be chosen from the given array an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+// It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+
+
+
+private void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
+        if (ind == arr.length) {
+            if (target == 0) {
+                ans.add(new ArrayList < > (ds));
+            }
+            return;
+        }
+
+        if (arr[ind] <= target) {
+            ds.add(arr[ind]);
+            findCombinations(ind, arr, target - arr[ind], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+        findCombinations(ind + 1, arr, target, ans, ds);
+    }
+    public List < List < Integer >> combinationSum(int[] candidates, int target) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        findCombinations(0, candidates, target, ans, new ArrayList < > ());
+        return ans;
+    }
