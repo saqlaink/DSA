306fa5344ed2cfb9d13673bf6400e7f32c78b722
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-21 07:58:38 +0530

    Stack and queue

--- stack and queue/QueueUsingStack.java
+++ stack and queue/QueueUsingStack.java
@@ -47,3 +47,52 @@ class Queue{
 		return input.size();
 	}
 }
+
+
+
+//Using two Stacks where push operation is O(1)
+
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Queue q = new Queue();
+        q.push(3);
+        q.push(4);
+        System.out.println("The element poped is " + q.pop());
+        q.push(5);
+        System.out.println("The top element is " + q.peek());
+        System.out.println("The size of the queue is " + q.size());
+    }
+}
+class Queue{
+	Stack<Integer> input;
+	Stack<Integer> output;
+	public Queue(){
+		input = new Stack<>();
+		output = new Stack<>();
+	}
+	void push(int ele){
+        System.out.println("The element pushed is " + ele);
+		input.push(ele);
+	}
+	int pop(){
+		if (output.empty()) {
+            while(!input.empty()){
+				output.push(input.pop());
+			}
+        }
+		return output.pop();
+	}
+	int peek(){
+		if (output.empty()) {
+            while(!input.empty()){
+				output.push(input.pop());
+			}
+        }
+		return output.peek();
+	}
+	int size(){
+		return input.size() + output.size();
+	}
+}
