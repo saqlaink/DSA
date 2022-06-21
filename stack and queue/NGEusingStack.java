b44a8de23ba3b3a9f3edaab2fcd8ad7b85b2b327
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-21 10:42:14 +0530

    Stack and queue

---
+++ stack and queue/NGEusingStack.java
@@ -0,0 +1,22 @@
+// Next Greater Element Using Stack
+// Problem Statement: Given a circular integer array A, return the next greater element for every element in A. The next greater element for an element x is the first element greater than x that we come across while traversing the array in a clockwise manner. If it doesn’t exist, return -1 for this element.
+
+
+public static int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int nge[] = new int[n];
+        Stack < Integer > st = new Stack < > ();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (st.isEmpty() == false && st.peek() <= nums[i % n]) {
+                st.pop();
+            }
+
+            if (i < n) {
+                if (st.isEmpty() == false) nge[i] = st.peek();
+                else nge[i] = -1;
+            }
+
+            st.push(nums[i % n]);
+        }
+        return nge;
+    }
