2fae1553bafc8c257b6051a8c8cb435784891a14
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-24 09:54:01 +0530

    Strings

---
+++ Strings/KMPAlgo.java
@@ -0,0 +1,35 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+public int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null || needle.length() > haystack.length()) return -1;
+        int[] parr = kmpPreprocess(needle);
+        int i = 0, j = 0;
+        while(i < haystack.length() && j < needle.length()) {
+            if(haystack.charAt(i) == needle.charAt(j)) {
+                i++; j++;
+            } else if(j > 0) {
+                j = parr[j - 1];
+            } else {
+                i++;
+            }
+        }
+        return j == needle.length() ? i - j : -1;
+    }
+    private int[] kmpPreprocess(String pattern){//figuring out is there a prefix equal to suffix
+//if there is no need to check from start check after prefix
+        int i = 1, j = 0;
+        int[] res = new int[pattern.length()];
+        while(i < pattern.length()) {
+            if(pattern.charAt(i) == pattern.charAt(j)) {
+                res[i] = j+1;
+                i++; j++;
+            } else if (j > 0) {
+                j = res[j-1];
+            } else {
+                res[i] = 0;
+                i++;
+            }
+        }
+        return res;
+    }
