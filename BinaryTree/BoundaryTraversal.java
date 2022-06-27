bd5b8c1f6fda5c6eb909e665eba6557ba86c64e9
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-27 16:18:20 +0530

    Binary Tree

---
+++ BinaryTree/BoundaryTraversal.java
@@ -0,0 +1,47 @@
+// 1. **Boundary Traversal of a Binary Tree**
+
+// **Problem Statement: BoundaryTraversal of a binary tree.** Write a program for the Anti-Clockwise Boundary traversal of a binary tree.
+
+
+static Boolean isLeaf(Node root) {
+        return (root.left == null) && (root.right == null);
+    }
+
+    static void addLeftBoundary(Node root, ArrayList < Integer > res) {
+        Node cur = root.left;
+        while (cur != null) {
+            if (isLeaf(cur) == false) res.add(cur.data);
+            if (cur.left != null) cur = cur.left;
+            else cur = cur.right;
+        }
+    }
+    static void addRightBoundary(Node root, ArrayList < Integer > res) {
+        Node cur = root.right;
+        ArrayList < Integer > tmp = new ArrayList < Integer > ();
+        while (cur != null) {
+            if (isLeaf(cur) == false) tmp.add(cur.data);
+            if (cur.right != null) cur = cur.right;
+            else cur = cur.left;
+        }
+        int i;
+        for (i = tmp.size() - 1; i >= 0; --i) {
+            res.add(tmp.get(i));
+        }
+    }
+
+    static void addLeaves(Node root, ArrayList<Integer> res){//inorder traversal
+        if (isLeaf(root)) {
+            res.add(root.data);
+            return;
+        }
+        if (root.left != null) addLeaves(root.left, res);
+        if (root.right != null) addLeaves(root.right, res);
+    }
+    static ArrayList < Integer > printBoundary(Node node) {
+        ArrayList < Integer > ans = new ArrayList < Integer > ();
+        if (isLeaf(node) == false) ans.add(node.data);
+        addLeftBoundary(node, ans);
+        addLeaves(node, ans);
+        addRightBoundary(node, ans);
+        return ans;
+    }
