a3504926c910ca382fcaea3dc3b2665de1fefc47
Saqlain Khan <iamsaqlain25@gmail.com>
2022-06-30 11:35:27 +0530

    Binary Search Tree

---
+++ BST/SearchBST.java
@@ -0,0 +1,11 @@
+// Search in a Binary Search Tree
+
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root!=null && root.val!= val){
+            root = val < root.val ? root.left : root.right;
+        }
+        return root;
+    }
+}
