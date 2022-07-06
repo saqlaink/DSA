713a1ce55de97453a522f423a1242df2d09c216b
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-06 21:17:31 +0530

    DP

--- DP/FrogJump.java
+++ DP/FrogJump.java
@@ -14,3 +14,20 @@ public static void main(String args[]) {
   Arrays.fill(dp,-1);
   System.out.println(solve(n-1,height,dp));
 }
+
+
+public static void main(String args[]) {
+  int height[]={30,10,60,10,60,50};
+  int n=height.length;
+  int dp[]=new int[n];
+  Arrays.fill(dp,-1);
+  dp[0]=0;
+  for(int ind=1;ind<n;ind++){
+      int jumpTwo = Integer.MAX_VALUE;
+        int jumpOne= dp[ind-1] + Math.abs(height[ind]-height[ind-1]);
+        if(ind>1)
+            jumpTwo = dp[ind-2] + Math.abs(height[ind]-height[ind-2]);
+        dp[ind]=Math.min(jumpOne, jumpTwo);
+  }
+  System.out.println(dp[n-1]);
+}
