682ea56b30779f4a1a7bc218b9ffdbcd68b29e73
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-06 22:49:46 +0530

    DP

--- DP/MaxSumNonAdjacent.java
+++ DP/MaxSumNonAdjacent.java
@@ -11,3 +11,19 @@ static int solve(int n,int[] arr){
     Arrays.fill(dp,-1);
     return solveUtil(n-1, arr, dp);
 }
+
+
+
+static int solve(int n, int[] arr){
+   int prev = arr[0];
+    int prev2 =0;
+    for(int i=1; i<n; i++){
+        int pick = arr[i];
+        if(i>1) pick += prev2;
+        int nonPick = 0 + prev;
+        int cur_i = Math.max(pick, nonPick);
+        prev2 = prev;
+        prev= cur_i;
+    }
+    return prev;
+}
