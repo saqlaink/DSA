86dcce78cf8c849fd5c33f6e60b297dbf685cf73
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-09 16:37:43 +0530

    DP

--- DP/MinPathSum.java
+++ DP/MinPathSum.java
@@ -12,3 +12,24 @@ static int minSumPath(int n, int m, int[][] matrix){
     Arrays.fill(row,-1);
     return minSumPathUtil(n-1,m-1,matrix,dp);
 }
+
+
+
+static int minSumPath(int n, int m, int[][] matrix){
+    int dp[][]=new int[n][m];
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<m; j++){
+            if(i==0 && j==0) dp[i][j] = matrix[i][j];
+            else{
+                int up = matrix[i][j];
+                if(i>0) up += dp[i-1][j];
+                else up += (int)Math.pow(10,9);
+                int left = matrix[i][j];
+                if(j>0) left+=dp[i][j-1];
+                else left += (int)Math.pow(10,9);
+                dp[i][j] = Math.min(up,left);
+            }
+        }
+    }
+    return dp[n-1][m-1];
+}
