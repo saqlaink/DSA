f53b0e1cea6b7a22a8606756ed7afec3a658a9e1
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-09 16:34:12 +0530

    DP

--- DP/GridUniquePath.java
+++ DP/GridUniquePath.java
@@ -11,3 +11,22 @@ static int countWays(int m, int n){
     for (int[] row : dp) Arrays.fill(row, -1);
     return countWaysUtil(m-1,n-1,dp);
 }
+
+
+static int countWaysUtil(int m, int n, int[][] dp) {
+ for(int i=0; i<m ;i++){
+      for(int j=0; j<n; j++){
+          //base condition
+          if(i==0 && j==0){
+              dp[i][j]=1;
+              continue;
+          }
+          int up=0;
+          int left = 0;
+          if(i>0) up = dp[i-1][j];
+          if(j>0) left = dp[i][j-1];
+          dp[i][j] = up+left;
+      }
+  }
+  return dp[m-1][n-1];
+}
