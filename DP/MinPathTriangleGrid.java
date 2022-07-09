f898ce3f5b74b1dac37c72d5b613b00512898f4e
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-09 16:38:31 +0530

    DP

--- DP/MinPathTriangleGrid.java
+++ DP/MinPathTriangleGrid.java
@@ -11,3 +11,19 @@ static int minimumPathSum(int[][] triangle, int n){
     Arrays.fill(row,-1);
     return mpsUtil(0,0,triangle,n,dp);
 }
+
+
+static int minimumPathSum(int[][] triangle, int n){
+    int dp[][]= new int[n][n];
+    for(int j=0;j<n;j++){
+        dp[n-1][j] = triangle[n-1][j];
+    }
+    for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            int down = triangle[i][j]+dp[i+1][j];
+            int diagonal = triangle[i][j]+dp[i+1][j+1];
+            dp[i][j] = Math.min(down, diagonal);
+        }
+    }
+    return dp[0][0];
+}
