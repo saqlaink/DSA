c60f6613f6c8e934d9af886b794832be65f5afbf
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-15 17:18:58 +0530

    DP

--- DP/CountPartitionWithSum.java
+++ DP/CountPartitionWithSum.java
@@ -19,3 +19,26 @@ public class Solution {
         return dp[idx][tar] = take+notTake;
     }
 }
+
+
+public static int findWays(int num[], int tar) {
+    int n = num.length;
+    int[][] dp = new int[n][tar+1];
+    for(int i=0;i<n;i++) dp[i][0] = 1;
+    if(num[0] <= tar) dp[0][num[0]] = 1;
+
+//for subsets with 0
+//if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+//else dp[0][0] = 1;  // 1 case - not pick
+//if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+
+    for(int idx=1;idx<n;idx++){
+        for(int sum=0;sum<=tar;sum++){
+            int notTake = dp[idx-1][sum];
+            int take = 0;
+            if(num[idx] <= sum) take = dp[idx-1][sum-num[idx]];
+            dp[idx][sum] = take+notTake;
+        }
+    }
+    return dp[n-1][tar];
+}
