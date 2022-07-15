1f7a4ad01cf8db8e3e670f0ce9da9e422da45b88
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-15 17:20:50 +0530

    DP

---
+++ DP/0-1Knacksap.java
@@ -0,0 +1,18 @@
+static int knapsackUtil(int[] wt,int[] val, int ind, int W,int[][] dp){
+    if(ind == 0){
+        if(wt[0] <=W) return val[0];
+        else return 0;
+    }
+    if(dp[ind][W]!=-1) return dp[ind][W];
+    int notTaken = 0 + knapsackUtil(wt,val,ind-1,W,dp);
+    int taken = Integer.MIN_VALUE;
+    if(wt[ind] <= W)
+        taken = val[ind] + knapsackUtil(wt,val,ind-1,W-wt[ind],dp);
+    return dp[ind][W] = Math.max(notTaken,taken);
+}
+static int knapsack(int[] wt,int[] val, int n, int W){
+    int dp[][]= new int[n][W+1];
+    for(int row[]: dp)
+    Arrays.fill(row,-1);
+    return knapsackUtil(wt, val, n-1, W, dp);
+}
