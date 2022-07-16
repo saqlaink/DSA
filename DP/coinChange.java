a90dfedc83848ef140a556b7f1a9992011d568f3
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-16 14:22:28 +0530

    DP

--- DP/coinChange.java
+++ DP/coinChange.java
@@ -16,3 +16,24 @@ static long countWaysToMakeChange(int[] arr, int n, int T){
     Arrays.fill(row,-1);
     return countWaysToMakeChangeUtil(arr,n-1, T, dp);
 }
+
+
+static long countWaysToMakeChange(int[] arr, int n, int T){
+    long dp[][]=new long[n][T+1]; 
+    //Initializing base condition
+    for(int i=0;i<=T;i++){
+        if(i%arr[0]==0) dp[0][i]=1;
+        // Else condition is automatically fulfilled,
+        // as dp array is initialized to zero
+    }
+    for(int ind=1; ind<n;ind++){
+        for(int target=0;target<=T;target++){
+            long notTaken = dp[ind-1][target];
+            long taken = 0;
+            if(arr[ind]<=target)
+                taken = dp[ind][target-arr[ind]];
+            dp[ind][target] = notTaken + taken;
+        }
+    }
+    return dp[n-1][T];
+}
