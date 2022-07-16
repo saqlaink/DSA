00198309fd72176908ba1560c7926c4cc35e11d3
Saqlain Khan <iamsaqlain25@gmail.com>
2022-07-16 14:24:06 +0530

    DP

--- DP/RodCutting.java
+++ DP/RodCutting.java
@@ -17,3 +17,24 @@ static int cutRod(int[] price,int N) {
     Arrays.fill(row,-1);
     return cutRodUtil(price,N-1,N,dp);
 }
+
+
+static int cutRod(int[] price,int N) {
+    int dp[][]=new int[N][N+1];
+    for(int row[]:dp)
+    Arrays.fill(row,-1);
+    for(int i=0; i<=N; i++){
+        dp[0][i] = i*price[0];
+    }
+    for(int ind=1; ind<N; ind++){
+        for(int length =0; length<=N; length++){
+             int notTaken = 0 + dp[ind-1][length];
+             int taken = Integer.MIN_VALUE;
+             int rodLength = ind+1;
+             if(rodLength <= length)
+                taken = price[ind] + dp[ind][length-rodLength];
+             dp[ind][length] = Math.max(notTaken,taken);   
+        }
+    }
+    return dp[N-1][N];
+}
