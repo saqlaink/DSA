32563af2538506631aa69d77528cb095bdce05d9
Saqlain Khan <iamsaqlain25@gmail.com>
2023-08-14 17:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (71.00%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,27 +1,14 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0, j = matrix[0].length - 1;
-        while(j>=0 && i<matrix.length){
-            if(target < matrix[i][j]) j--;
-            else if(target > matrix[i][j]) i++;
-            else return true;
+        if(matrix.length == 0) return false;
+        int n = matrix.length, m = matrix[0].length;
+        int lo = 0, hi = (n*m) - 1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(matrix[mid/m][mid%m] == target) return true;
+            else if(matrix[mid/m][mid%m] < target) lo = mid+1;
+            else hi = mid-1; 
         }
         return false;
     }
 }
-// class Solution {
-//     public boolean searchMatrix(int[][] matrix, int target) {
-//         int i = 0;
-//         int j = matrix[0].length - 1;
-//         while(j>=0 && i<matrix.length){
-//             if(target < matrix[i][j]){
-//                 j--;
-//             }else if(target > matrix[i][j]){
-//                 i++;
-//             }else{
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// }
