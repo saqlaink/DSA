92a2e1d9dcc6a46c93b4079457db24f34ab137dd
Saqlain Khan <iamsaqlain25@gmail.com>
2023-08-14 16:44:15 +0530

    Time: 0 ms (100%), Space: 40.8 MB (82%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+        while(j>=0 && i<matrix.length){
+            if(target < matrix[i][j]) j--;
+            else if(target > matrix[i][j]) i++;
+            else return true;
+        }
+        return false;
+    }
+}
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int i = 0;
+//         int j = matrix[0].length - 1;
+//         while(j>=0 && i<matrix.length){
+//             if(target < matrix[i][j]){
+//                 j--;
+//             }else if(target > matrix[i][j]){
+//                 i++;
+//             }else{
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
