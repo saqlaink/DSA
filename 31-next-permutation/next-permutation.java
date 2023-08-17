430582352e38edd103367e507e78f3fd9d48a947
Saqlain Khan <iamsaqlain25@gmail.com>
2023-08-17 16:17:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (94.35%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if(nums == null || nums.length <=1) return;
+        int i = nums.length - 2;
+        while(i >= 0 && nums[i] >= nums[i+1]) i--;
+        int j = nums.length - 1;
+        if(i>=0){
+            while(nums[j] <= nums[i]) j--;
+            swap(nums,i,j);
+        }
+        reverse(i+1,nums.length - 1,nums);
+    }
+    public void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void reverse(int i,int j,int[] nums){
+        while(i<j) swap(nums,i++,j--);
+    }
+}
