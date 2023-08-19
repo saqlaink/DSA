35faf287727717595ce10b0efd783df817d5cab5
Saqlain Khan <iamsaqlain25@gmail.com>
2023-08-19 21:07:32 +0530

    Time: 1 ms (98.87%), Space: 40.7 MB (51.41%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean areSentencesSimilar(String s1, String s2) {
-        
         if (s1.length () < s2.length ()) {
             return areSentencesSimilar(s2, s1);
         }
         String[] w1 = s1.split(" "), w2 = s2.split(" ");
-        int n1 = w1.length, n2 = w2.length;
-        int i = 0, j = n1 - 1, k = n2 - 1;
-        while(i < n2 && w1[i].equals(w2[i])) i++;
-        while(k >= 0 && w1[j].equals(w2[k])){
-            j--;k--;
+        int i1 = 0, i2 = 0;
+        int j1 = w1.length - 1, j2 = w2.length - 1;
+        while(i2 <= j2){
+            if(w1[i1].equals(w2[i2])){
+                i1++;i2++;
+            }else if(w1[j1].equals(w2[j2])){
+                j1--;j2--;
+            }else return false;
         }
-        return i > k;
+        return true;
     }
 }
