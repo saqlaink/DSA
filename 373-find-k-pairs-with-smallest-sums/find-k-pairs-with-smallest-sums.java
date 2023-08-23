98274cbf6eaab3cc3c4774de16032ef492349390
Saqlain Khan <iamsaqlain25@gmail.com>
2023-08-23 17:43:09 +0530

    Time: 32 ms (95.53%) | Memory: 56.4 MB (87.50%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,23 +1,33 @@
 class Solution {
-    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k){
-        List<List<Integer>> res = new ArrayList<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
-        for(int x: nums1){
-            pq.add(new int[]{x+nums2[0], 0}); //sum & idx of num2
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> resV = new ArrayList<>(); // Result list to store the pairs
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        // Priority queue to store pairs with smallest sums, sorted by the sum
+
+        // Push the initial pairs into the priority queue
+        for (int x : nums1) {
+            pq.offer(new int[]{x + nums2[0], 0}); // The sum and the index of the second element in nums2
         }
-        while(k>0 && !pq.isEmpty()){
+
+        // Pop the k smallest pairs from the priority queue
+        while (k > 0 && !pq.isEmpty()) {
             int[] pair = pq.poll();
-            int sum = pair[0];
-            int idx = pair[1];
+            int sum = pair[0]; // Get the smallest sum
+            int pos = pair[1]; // Get the index of the second element in nums2
+
             List<Integer> currentPair = new ArrayList<>();
-            currentPair.add(sum - nums2[idx]);
-            currentPair.add(nums2[idx]);
-            res.add(currentPair);
-            if(idx+1 < nums2.length){
-                pq.add(new int[]{sum - nums2[idx] + nums2[idx+1], idx+1});
+            currentPair.add(sum - nums2[pos]);
+            currentPair.add(nums2[pos]);
+            resV.add(currentPair); // Add the pair to the result list
+
+            // If there are more elements in nums2, push the next pair into the priority queue
+            if (pos + 1 < nums2.length) {
+                pq.offer(new int[]{sum - nums2[pos] + nums2[pos + 1], pos + 1});
             }
-            k--;
+
+            k--; // Decrement k
         }
-        return res;
+
+        return resV; // Return the k smallest pairs
     }
 }
