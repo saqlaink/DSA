680ee07b15b1064c3cd22a5f7660fc315f083098
Saqlain Khan <iamsaqlain25@gmail.com>
2023-09-02 20:44:45 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (27.64%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -4,32 +4,28 @@ class Solution {
         sum(0,arr,k,res,new ArrayList<>());
         return res;
     }
-    public static void sum(int idx,int[] arr,int k,List<List<Integer>> res,ArrayList<Integer> ds){
+    static void sum(int idx,int[] arr,int k,List<List<Integer>> res,ArrayList<Integer> ds){
+        // if(idx == arr.length){
+        //     if(k == 0){
+        //         res.add(new ArrayList<>(ds));
+        //     }
+        //     return;
+        // }
+        // if(arr[idx] <= k){
+        //     ds.add(arr[idx]);
+        //     sum(idx,arr,k-arr[idx],res,ds);
+        //     ds.remove(ds.size() - 1);
+        // }
+        // sum(idx+1,arr,k,res,ds);
         if(idx == arr.length){
-            if(k == 0){
-                res.add(new ArrayList<>(ds));
-            }
+            if(k == 0) res.add(new ArrayList<>(ds));
             return;
         }
         if(arr[idx] <= k){
             ds.add(arr[idx]);
-            sum(idx,arr,k-arr[idx],res,ds);
+            sum(idx,arr,k - arr[idx],res,ds);
             ds.remove(ds.size() - 1);
         }
         sum(idx+1,arr,k,res,ds);
     }
 }
-    // static void combinationSum(int idx,int[] arr,int k,List<List<Integer>> ans,ArrayList<Integer> ds){
-    //     if(idx==arr.length){
-    //         if(k == 0){
-    //             ans.add(new ArrayList<>(ds));
-    //         }
-    //         return;
-    //     }
-    //     if(arr[idx] <= k){
-    //         ds.add(arr[idx]);
-    //         combinationSum(idx,arr,k-arr[idx],ans,ds);
-    //         ds.remove(ds.size()-1);
-    //     }
-    //     combinationSum(idx+1,arr,k,ans,ds);
-    // }
