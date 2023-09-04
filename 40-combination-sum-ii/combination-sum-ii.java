da9632e09eb185abb7737b1b7a35b4f5de3c70da
Saqlain Khan <iamsaqlain25@gmail.com>
2023-09-04 21:59:53 +0530

    Time: 2 ms (99.77%) | Memory: 41.9 MB (98.14%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -6,15 +6,26 @@ class Solution {
         return res;
     }
     public static void sum(int idx,int[] arr,int k,List<List<Integer>> res,ArrayList<Integer> ds){
+        // if(k == 0){
+        //     res.add(new ArrayList<>(ds));
+        //     return;
+        // }
+        // for(int i=idx;i<arr.length;i++){
+        //     if(i>idx && arr[i] == arr[i-1]) continue;
+        //     if(arr[i] > k) break;
+        //     ds.add(arr[i]);
+        //     sum(i+1,arr,k-arr[i],res,ds);
+        //     ds.remove(ds.size() - 1);
+        // }
         if(k == 0){
             res.add(new ArrayList<>(ds));
             return;
         }
         for(int i=idx;i<arr.length;i++){
-            if(i>idx && arr[i] == arr[i-1]) continue;
+            if(i > idx && arr[i] == arr[i-1]) continue;
             if(arr[i] > k) break;
             ds.add(arr[i]);
-            sum(i+1,arr,k-arr[i],res,ds);
+            sum(i+1,arr,k - arr[i], res,ds);
             ds.remove(ds.size() - 1);
         }
     }
