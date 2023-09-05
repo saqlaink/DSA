be8e314927f7d6d28e668f4c0217f7894cf83bda
Saqlain Khan <iamsaqlain25@gmail.com>
2023-09-05 08:43:40 +0530

    Time: 5 ms (48.88%) | Memory: 41.7 MB (10.51%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -10,25 +10,37 @@ class Solution {
         '9',"wxyz"
     );
     public List<String> letterCombinations(String digits) {
-        if (digits.length() == 0) {
-            return new ArrayList();
-        }
-        List<String> ans = new ArrayList();
-        String cur = "";
-        backtrack(digits, ans, cur, 0);
+        if(digits.length() == 0) return new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+        String curr = "";
+        backtrack(0,digits,ans,curr);
         return ans;
     }
-    public void backtrack(String digits, List<String> ans, String cur,int index) {
-        if (cur.length() == digits.length()) {
-            ans.add(cur);
+    public void backtrack(int idx,String digits,List<String> ans,String curr){
+        if(curr.length() == digits.length()){
+            ans.add(curr);
             return;
-        } else if (index >= digits.length()) {
-            return;
-        } else {
-            String digit = digitToChar.get(digits.charAt(index));
-            for (char c : digit.toCharArray()) {
-                backtrack(digits, ans, cur + c, index + 1);
+        }else if(idx >= digits.length()) return;
+        else{
+            String digit = digitToChar.get(digits.charAt(idx));
+            for(char c: digit.toCharArray()){
+                backtrack(idx+1,digits,ans,curr+c);
             }
         }
     }
 }
+// class Solution {
+//     public void backtrack(String digits, List<String> ans, String cur,int index) {
+//         if (cur.length() == digits.length()) {
+//             ans.add(cur);
+//             return;
+//         } else if (index >= digits.length()) {
+//             return;
+//         } else {
+//             String digit = digitToChar.get(digits.charAt(index));
+//             for (char c : digit.toCharArray()) {
+//                 backtrack(digits, ans, cur + c, index + 1);
+//             }
+//         }
+//     }
+// }
