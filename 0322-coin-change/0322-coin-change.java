af20d7f5e57c0a11a0960434f5cf0dfa94057462
Saqlain Khan <iamsaqlain25@gmail.com>
2023-10-06 15:08:50 +0530

    Time: 27 ms (23.46%), Space: 43.6 MB (37.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,20 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int [][] dp = new int[n+1][amount+1];
-        for(int[] a: dp) Arrays.fill(a,-1); 
+        int[][] dp = new int[n+1][amount+1];
+        for(int[] a:dp) Arrays.fill(a,-1);
         int ans = solve(n-1,amount,coins,dp);
-        if(ans >= Integer.MAX_VALUE-1) {
-            System.out.println(-1);
-            return -1;
-        }
+        if(ans >= Integer.MAX_VALUE - 1) return -1;
         return ans;
     }
     public static int solve(int n,int s,int[] arr,int[][] dp){
         if(n == 0){
             if(s%arr[0] == 0) return s/arr[0];
-            else return Integer.MAX_VALUE-1;
+            else return Integer.MAX_VALUE - 1;
         }
         if(dp[n][s] != -1) return dp[n][s];
         int not = solve(n-1,s,arr,dp);
-        int take = Integer.MAX_VALUE-1;
+        int take = Integer.MAX_VALUE - 1;
         if(arr[n] <= s) take = 1 + solve(n,s-arr[n],arr,dp);
         return dp[n][s] = Math.min(not,take);
     }
