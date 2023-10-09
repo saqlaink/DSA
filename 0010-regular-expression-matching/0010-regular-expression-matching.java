ce7ccf357d21c4757a35f174c0918f9b31c2f3f9
Saqlain Khan <iamsaqlain25@gmail.com>
2023-10-09 07:03:09 +0530

    Time: 490 ms (14.18%), Space: 41.3 MB (28.49%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -4,15 +4,27 @@ class Solution {
         return isMatch(0,0,s,p,dp);
     }
     public boolean isMatch(int i, int j,String s, String p,boolean[][] dp){
-        if(s.length() <= i && j >= p.length()) return true;
+//         if(s.length() <= i && j >= p.length()) return true;
+//         if(j>=p.length()) return false;
+//         if(dp[i][j] != false) return true;
+// //declaring a variable to check whether the first characters of s and p are a match or not
+//         boolean match = i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+//         //checking for second character
+//         if((j+1) < p.length() && p.charAt(j+1) == '*'){
+//             boolean notTake = isMatch(i,j+2,s,p,dp);
+//             boolean take = match ? isMatch(i+1,j,s,p,dp) : false;
+//             return dp[i][j] = take || notTake;
+//         }
+//         if(match) return dp[i][j] = isMatch(i+1,j+1,s,p,dp);
+//         return dp[i][j] = false;
+        if(s.length() <= i && j>=p.length()) return true;
         if(j>=p.length()) return false;
         if(dp[i][j] != false) return true;
-
         boolean match = i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
         if((j+1) < p.length() && p.charAt(j+1) == '*'){
-            boolean notTake = isMatch(i,j+2,s,p,dp);
+            boolean not = isMatch(i,j+2,s,p,dp);
             boolean take = match ? isMatch(i+1,j,s,p,dp) : false;
-            return dp[i][j] = take || notTake;
+            return dp[i][j] = take || not;
         }
         if(match) return dp[i][j] = isMatch(i+1,j+1,s,p,dp);
         return dp[i][j] = false;
