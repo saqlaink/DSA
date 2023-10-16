50016c8831e8e9861688f0b92e04b51bbc2abe59
Saqlain Khan <iamsaqlain25@gmail.com>
2023-10-16 15:28:05 +0530

    Time: 103 ms (45.07%), Space: 67.9 MB (32.94%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,24 @@
+class MedianFinder {
+    PriorityQueue<Integer> small;
+    PriorityQueue<Integer> large;
+    boolean even;
+    public MedianFinder() {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        large = new PriorityQueue<>();
+        even = true;
+    }
+    public void addNum(int num) {
+        if(even){
+            large.offer(num);
+            small.offer(large.poll());
+        }else{
+            small.offer(num);
+            large.offer(small.poll());
+        }
+        even = !even;
+    }
+    public double findMedian() {
+        if(even) return (small.peek() + large.peek()) / 2.0;
+        else return small.peek();
+    }
+}
